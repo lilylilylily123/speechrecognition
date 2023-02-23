d9344e22da81aa93e7456aedd6a30b0614b236ec
lily <102743831+lilylilylily123@users.noreply.github.com>
2023-02-23 09:13:43 +0100

    i hope this works

---
+++ test1.py
@@ -0,0 +1,8 @@
+from test2 import testers
+
+
+def testir():
+    print(testers.__init__(self="ajajjda"))
+    print(testers.__init__())
+
+testir()
---
+++ test2.py
@@ -0,0 +1,15 @@
+global hhh
+hhh = "abcdef"
+class testers():
+    def __init__(self):
+        def hello(self):
+            global hhh
+            aa = "bbbb"
+            print(aa)
+            print(hhh)
+            hhh = "hello!"
+            print(hhh)
+
+
+        hello(self)
+
